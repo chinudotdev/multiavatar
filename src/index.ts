5ec07be41561a565274cbfe623064997f29f4fc4
chinu <chinudotdev@gmail.com>
2025-09-23 20:04:45 +0530

    fix: add cache

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,27 @@ app.get("/", (c) => {
   return c.text("OK");
 });
 
-app.get("/:id", (c) => {
+app.get("/:id", async (c) => {
   const id = c.req.param("id");
+  const cache = caches.default;
+  let response = await cache.match(id);
+  if (response) {
+    return response;
+  }
+
   const avatar = multiavatar(id, true);
   c.header("Content-Type", "image/svg+xml");
   c.header("Cache-Control", "public, max-age=31536000, immutable");
 
-  c.header("Vary", "Accept-Encoding");
-  c.header("ETag", `"avatar-${id}"`);
-  c.header("Last-Modified", new Date().toUTCString());
-
-  // Additional headers to ensure caching
-  c.header("X-Content-Type-Options", "nosniff");
-  c.header("Access-Control-Max-Age", "31536000");
+  await cache.put(
+    id,
+    new Response(avatar, {
+      headers: {
+        "Content-Type": "image/svg+xml",
+        "Cache-Control": "public, max-age=31536000, immutable",
+      },
+    })
+  );
 
   return c.body(avatar);
 });
